e3ffcc080757b7422ecaaa288e50a6c43a08e339
Aparna Tati <aparna.t@simformsolutions.com>
2023-03-01 15:31:44 +0530

    Project Setup

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Aparna Tati on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
