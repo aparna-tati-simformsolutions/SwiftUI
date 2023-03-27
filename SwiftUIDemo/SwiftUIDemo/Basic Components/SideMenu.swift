9486c5e92b4e67dd05a4d41148beee53b5290924
Aparna Tati <aparna.t@simformsolutions.com>
2023-03-27 14:28:05 +0530

    Sidebar changes

--- SwiftUIDemo/SwiftUIDemo/Basic Components/SideMenu.swift
+++ SwiftUIDemo/SwiftUIDemo/Basic Components/SideMenu.swift
@@ -10,20 +10,18 @@ import SwiftUI
 struct MenuItem: Identifiable {
     var id = UUID()
     let text: String
-    let handler: () -> Void = {
-        print("Tapped Item")
-    }
+    let image: String
 }
 
 struct MenuContent: View {
     
     let items: [MenuItem] = [
-        MenuItem(text: "Home"),
-        MenuItem(text: "Settings"),
-        MenuItem(text: "Profile"),
-        MenuItem(text: "Activty"),
-        MenuItem(text: "Flights"),
-        MenuItem(text: "Share")
+        MenuItem(text: "Home", image: "house"),
+        MenuItem(text: "Settings", image: "gearshape.fill"),
+        MenuItem(text: "Profile", image: "person.crop.circle"),
+        MenuItem(text: "Notifications", image: "bell"),
+        MenuItem(text: "Flight Mode", image: "airplane"),
+        MenuItem(text: "Share", image: "square.and.arrow.up")
     ]
     
     var body: some View {
@@ -31,8 +29,16 @@ struct MenuContent: View {
             Color(UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1))
             
             VStack(alignment: .leading, spacing: 0) {
+                Spacer()
+                    .frame(height: 50)
                 ForEach(items) { item in
                     HStack {
+                        Image(systemName: item.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.white)
+                            .frame(width: 30, height: 30, alignment: .center)
+                        
                         Text(item.text)
                             .bold()
                             .multilineTextAlignment(.leading)
@@ -58,6 +64,7 @@ struct SideMenuView: View {
     let width: CGFloat
     let menuOpened: Bool
     let toggleMenu: () -> Void
+    @Binding var isBackButtonHide: Bool
     
     var body: some View {
         ZStack {
@@ -69,6 +76,7 @@ struct SideMenuView: View {
             .opacity(menuOpened ? 1 : 0)
             .animation(Animation.easeIn.delay(0.25))
             .onTapGesture {
+                isBackButtonHide = false
                 self.toggleMenu()
             }
             
@@ -87,12 +95,14 @@ struct SideMenuView: View {
 struct SideMenu: View {
     
     @State private var isMenuOpened = false
+    @State private var isBackButtonHide = false
     
     var body: some View {
         ZStack {
             if !isMenuOpened {
                 Button {
                     self.isMenuOpened.toggle()
+                    isBackButtonHide = true
                 } label: {
                     Text("Open Menu")
                         .frame(width: 200, height: 50, alignment: .center)
@@ -101,9 +111,9 @@ struct SideMenu: View {
                         .cornerRadius(10)
                 }
             }
-            
-            SideMenuView(width: UIScreen.main.bounds.width / 1.5, menuOpened: isMenuOpened, toggleMenu: toggleMenu)
+            SideMenuView(width: UIScreen.main.bounds.width / 1.5, menuOpened: isMenuOpened, toggleMenu: toggleMenu, isBackButtonHide: $isBackButtonHide)
         }.edgesIgnoringSafeArea(.all)
+        .navigationBarBackButtonHidden(isBackButtonHide)
     }
     
     func toggleMenu() {
